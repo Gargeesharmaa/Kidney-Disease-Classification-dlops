b6b14b59180d1a162c18ea265328ddf34c7388e4
Gargee Sharma <gargeesharma52@gmail.com>
2026-07-14 03:47:55 +0530

    added data_ingestion component

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-from src.Kidney_Disease_Classification import logger
+import os
+import sys
 
-logger.info("welcome to the cnn Classication prediction model")
+# Point Python to the 'src' directory directly
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+# Now these imports will work perfectly!
+from Kidney_Disease_Classification import logger
+from Kidney_Disease_Classification.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+
+from Kidney_Disease_Classification import logger
+from Kidney_Disease_Classification.pipeline.stage1_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
